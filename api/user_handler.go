9a043c2a64e08a1ad7e78af47e09d6c6147d2e67
PrayasPathak <prayasroman1@gmail.com>
2024-09-19 16:20:26 +0545

    Add database connection

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,11 +1,31 @@
 package api
 
 import (
+	"github.com/PrayasPathak/hotel-reservation/db"
 	"github.com/PrayasPathak/hotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user, err := h.userStore.GetUserById(c.Context(), id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	users := []types.User{
 		{FirstName: "John", LastName: "Smith"},
 		{FirstName: "John", LastName: "Doe"},
@@ -14,15 +34,5 @@ func HandleGetUsers(c *fiber.Ctx) error {
 	}
 	return c.JSON(map[string][]types.User{
 		"users": users,
-	})
-}
-
-func HandleGetUser(c *fiber.Ctx) error {
-	user := types.User{
-		FirstName: "Matt",
-		LastName:  "Henry",
-	}
-	return c.JSON(map[string]types.User{
-		"user": user,
 	})
 }
--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/PrayasPathak/hotel-reservation/api"
+	"github.com/PrayasPathak/hotel-reservation/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const (
+	dbUri          = "mongodb://localhost:27017"
+	dbName         = "hotel-reservation"
+	userCollection = "users"
+)
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
+
 	listenAddr := flag.String("listenAddr", ":5000", "Port at which the api listens")
 	flag.Parse()
 
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
+	if err != nil {
+		log.Fatal(err)
+	}
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
