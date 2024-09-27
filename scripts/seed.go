1c72df2b030a8496c6ba377fc5bcfd683f97860c
PrayasPathak <prayasroman1@gmail.com>
2024-09-27 07:05:55 +0545

    Refactor the seeding script

--- scripts/seed.go
+++ scripts/seed.go
@@ -8,108 +8,34 @@ import (
 
 	"github.com/PrayasPathak/hotel-reservation/api"
 	"github.com/PrayasPathak/hotel-reservation/db"
-	"github.com/PrayasPathak/hotel-reservation/types"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/PrayasPathak/hotel-reservation/db/fixtures"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var (
-	client       *mongo.Client
-	roomStore    db.RoomStore
-	hotelStore   db.HotelStore
-	userStore    db.UserStore
-	bookingStore db.BookingStore
-	ctx          = context.Background()
-)
-
-func seedUser(fname, lname, email, password string, isAdmin bool) *types.User {
-	user, err := types.NewUserFromParams(types.CreateUserParams{
-		FirstName: fname,
-		LastName:  lname,
-		Email:     email,
-		Password:  password,
-	})
-	user.IsAdmin = isAdmin
-	if err != nil {
-		log.Fatal(err)
-	}
-	insertedUser, err := userStore.InsertUser(context.TODO(), user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s -> %s\n", user.Email, api.CreateTokenFromUser(user))
-	return insertedUser
-}
-
-func seedRoom(size string, ss bool, price float64, hotelID primitive.ObjectID) *types.Room {
-	room := &types.Room{
-		Size:    size,
-		Seaside: ss,
-		Price:   price,
-		HotelID: hotelID,
-	}
-
-	inseretedRoom, err := roomStore.InsertRoom(context.Background(), room)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("RoomID: %s \n", room.ID)
-	return inseretedRoom
-}
-
-func seedBooking(userID, roomID primitive.ObjectID, from, till time.Time) {
-	booking := &types.Booking{
-		UserID:   userID,
-		RoomID:   roomID,
-		FromDate: from,
-		TillDate: till,
-	}
-	insertedBooking, err := bookingStore.InsertBooking(context.Background(), booking)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Inserted Booking: %+v\n", insertedBooking)
-}
-
-func seedHotel(name, location string, rating int) *types.Hotel {
-	hotel := types.Hotel{
-		Name:     name,
-		Location: location,
-		Rooms:    []primitive.ObjectID{},
-		Rating:   rating,
-	}
-
-	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return insertedHotel
-}
-
 func main() {
-	ramesh := seedUser("Ramesh", "Sharma", "ramesh@gmail.com", "sharmaramesh", false)
-	seedUser("admin", "admin", "admin@admin.com", "admin123", true)
-	h1 := seedHotel("Hotel Trisara", "Nepal", 4)
-	h2 := seedHotel("Hotel Annapurna", "Nepal", 4)
-	seedRoom("small", true, 89.99, h1.ID)
-	seedRoom("meduim", true, 149.99, h1.ID)
-	seedRoom("kingsize", false, 229.99, h2.ID)
-	room := seedRoom("kingsize", true, 289.99, h2.ID)
-	seedBooking(ramesh.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 4))
-}
-
-func init() {
+	ctx := context.Background()
 	var err error
-	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
 	if err != nil {
 		log.Fatal(err)
 	}
 	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
 		log.Fatal(err)
 	}
-	hotelStore = db.NewMongoHotelStore(client)
-	roomStore = db.NewMongoRoomStore(client, hotelStore)
-	userStore = db.NewMongoUserStore(client)
-	bookingStore = db.NewMongoBookingStore(client)
+	hotelStore := db.NewMongoHotelStore(client)
+	store := &db.Store{
+		User:    db.NewMongoUserStore(client),
+		Booking: db.NewMongoBookingStore(client),
+		Room:    db.NewMongoRoomStore(client, hotelStore),
+		Hotel:   hotelStore,
+	}
+	user := fixtures.AddUser(store, "mike", "daniel", false)
+	fmt.Println("Mike -> ", api.CreateTokenFromUser(user))
+	admin := fixtures.AddUser(store, "admin", "admin", true)
+	fmt.Println("Admin -> ", api.CreateTokenFromUser(admin))
+	hotel := fixtures.AddHotel(store, "some hotel", "bermuda", 5, nil)
+	room := fixtures.AddRoom(store, "large", true, 299.99, hotel.ID)
+	booking := fixtures.AddBooking(store, room.ID, user.ID, time.Now(), time.Now().AddDate(0, 0, 5))
+	fmt.Printf("Booking -> %+v\n", booking)
 }
